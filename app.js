11e6008cb3b4814c02468d39bb4168d38fd22ab2
Roman Zhigaikov <zhigaikov.roman@yandex.ru>
2020-04-07 18:49:50 +0300

    first review

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const usersRouter = require('./routes/users.js');
+const cardsRouter = require('./routes/cards.js');
 
 const { PORT = 3000 } = process.env;
 
@@ -7,3 +9,10 @@ const app = express();
 app.listen(PORT, () => {
     console.log('test');
 });
+
+app.use('/', usersRouter);
+app.use('/', cardsRouter);
+app.use('/', (req, res) => {
+    res.status(404).json({ message: 'Запрашиваемый ресурс не найден' });
+});
+app.use(express.static((__dirname, 'public')));
