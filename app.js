38816f3440a88e376e2383b091e88772a3dfdfa8
Roman Zhigaikov <zhigaikov.roman@yandex.ru>
2020-05-22 17:21:21 +0300

    version 1.3.2

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
-//   avatar: Joi.string().required().uri(),
+    avatar: Joi.string().required().regex(/(http:\/\/|https:\/\/)(www\.)?(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|(\w|-)+\.(\w|-)+(\.(\w|-))?)(:\d{1,5})?[a-zA-Z0-9/_-]+#?(\.\w+)?/im),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
   }).unknown(true),
